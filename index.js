7e7cc724793d5d68c0befefaf28f78b720ccf95f
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2022-01-15 17:08:17 +0100

    start

---
+++ index.js
@@ -0,0 +1,17 @@
+const { ApolloServer } = require('apollo-server');
+const { ApolloGateway } = require('@apollo/gateway');
+
+const supergraphSdl = ''; // TODO!
+
+const gateway = new ApolloGateway({
+  supergraphSdl
+});
+
+const server = new ApolloServer({
+  gateway,
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Gateway ready at ${url}`);
+}).catch(err => {console.error(err)});
+
